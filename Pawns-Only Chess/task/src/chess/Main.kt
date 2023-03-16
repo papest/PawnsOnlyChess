2d03bb50ad3b54e221cf9c100975f3b001486490
Epap <peia@inbox.ru>
2023-03-16 20:39:45 +0300

    Stage 4/5: Capture

--- Pawns-Only Chess/task/src/chess/Main.kt
+++ Pawns-Only Chess/task/src/chess/Main.kt
@@ -1,11 +1,15 @@
 package chess
 
+import kotlin.math.abs
+
 object ChessBoard {
-    const val SIZE = 8
+    private const val SIZE = 8
     private const val TITLE = "Pawns-Only Chess"
 
     private val horizontalBorder = "+---".repeat(SIZE) + "+"
     private val bottomNumbers = "    ${('a' until 'a' + SIZE).joinToString("   ")}"
+    private const val lastLetter = 'a' + SIZE - 1
+    private val regExp = "[a-$lastLetter][1-$SIZE][a-$lastLetter][1-$SIZE]".toRegex()
     val chessBoard = mutableListOf<MutableList<Char>>()
 
     init {
@@ -37,12 +41,10 @@ object ChessBoard {
         println(TITLE)
     }
 
-    private fun fileToInt(file: Char): Int = file - 'a'
-    private fun rankToInt(rank: Char): Int = SIZE - (rank - '0')
+    fun fileToInt(file: Char): Int = file - 'a'
+    fun rankToInt(rank: Char): Int = SIZE - (rank - '0')
 
-    private fun turn(army: Army): String {
-        val lastLetter = 'a' + SIZE - 1
-        val regExp = "[a-$lastLetter][1-$SIZE][a-$lastLetter][1-$SIZE]".toRegex()
+    private fun turn(army: Army, lastTurn: String): String {
 
         while (true) {
             println("${army.userName}'s turn:")
@@ -50,14 +52,25 @@ object ChessBoard {
             if (turn == "exit") return turn
             if (turn.matches(regExp)) {
                 try {
-                    army.forward(
-                        fileToInt(turn[0]),
-                        rankToInt(turn[1]),
-                        fileToInt(turn[2]),
-                        rankToInt(turn[3])
-                    )
-                    chessBoard[rankToInt(turn[1])][fileToInt(turn[0])] = ' '
-                    chessBoard[rankToInt(turn[3])][fileToInt(turn[2])] = army.symbol
+                    val f1 = fileToInt(turn[0])
+                    val f2 = fileToInt(turn[2])
+                    val r1 = rankToInt(turn[1])
+                    val r2 = rankToInt(turn[3])
+
+                    if (chessBoard[r1][f1] != army.symbol)
+                        throw Exception("No ${army.name.lowercase()} pawn at ${'a' + f1}${SIZE - r1}")
+
+                    if (f1 != f2) {
+                        if (chessBoard[r2][f2] != ' ') army.capture(f1, r1, f2, r2)
+                        else {
+                            army.enPassant(f1, r1, f2, r2, lastTurn)
+                            chessBoard[rankToInt(lastTurn[3])][fileToInt(lastTurn[2])] = ' '
+                        }
+
+                    } else army.forward(f1, r1, f2, r2)
+
+                    chessBoard[r1][f1] = ' '
+                    chessBoard[r2][f2] = army.symbol
                     return turn
 
                 } catch (e: Exception) {
@@ -76,10 +89,13 @@ object ChessBoard {
         printChessBoard()
 
         var next = 0
+        var lastTurn = ""
 
         while (true) {
             next %= 2
-            if (turn(Army.values()[next++]) == "exit") break
+            val turn = turn(Army.values()[next++], lastTurn)
+            if (turn == "exit") break
+            lastTurn = turn
             printChessBoard()
         }
 
@@ -105,15 +121,26 @@ enum class Army(
     BLACK('B', "", 1, 1, 2);
 
     fun forward(fromFile: Int, fromRank: Int, toFile: Int, toRank: Int) {
-        if (ChessBoard.chessBoard[fromRank][fromFile] != symbol)
-            throw Exception("No ${this.name.lowercase()} pawn at ${'a' + fromFile}${ChessBoard.SIZE - fromRank}")
         if (ChessBoard.chessBoard[toRank][toFile] != ' ') throw Exception("Invalid input")
-        if (fromFile != toFile) throw Exception("Invalid input")
-        if (forward == toRank - fromRank || (extraPosition == fromRank && extraForward == toRank - fromRank)) return
+        if (fromFile == toFile &&
+            (forward == toRank - fromRank || (extraPosition == fromRank && extraForward == toRank - fromRank))
+        ) return
         else throw Exception("Invalid input")
     }
-}
 
+    fun enPassant(fromFile: Int, fromRank: Int, toFile: Int, toRank: Int, lastTurn: String) {
+        if (lastTurn.isNotEmpty()
+            && ChessBoard.rankToInt(lastTurn[3]) == fromRank && ChessBoard.fileToInt(lastTurn[2]) == toFile
+            && abs(toFile - fromFile) == 1 && (toRank - fromRank == forward)
+        ) return
+        else throw Exception("Invalid input")
+    }
+
+    fun capture(fromFile: Int, fromRank: Int, toFile: Int, toRank: Int) {
+        if (toRank - fromRank == forward && abs(toFile - fromFile) == 1) return
+        throw Exception("Invalid input")
+    }
+}
 
 fun main() {
     ChessBoard.play()
